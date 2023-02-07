0b656c90bcb0b37a0a7591fe7baac410761ff6a0
gavin3567 <gavinostheimer00@gmail.com>
2023-02-06 19:49:57 -0700

    Branch Commit

---
+++ Assignment4/Controllers/BlahController.cs
@@ -0,0 +1,30 @@
+﻿using Assignment4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment4.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Assignment4/Models/GradeCalculatorModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment4.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Required]
+        [Range(0, 100, ErrorMessage = "Assignment must be between 0 and 100")]
+        public int Assignment { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Group must be between 0 and 100")]
+        public int Group { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Quiz must be between 0 and 100")]
+        public int Quiz { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Midterm must be between 0 and 100")]
+        public int Midterm { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Final must be between 0 and 100")]
+        public int Final { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Intex must be between 0 and 100")]
+        public int Intex { get; set; }
+
+    }
+}
